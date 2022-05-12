f43f4392706a8a9d09edc9f985c435cd4037e04d
JoeyYoung <Cookie_8421@sina.cn>
2022-05-12 21:01:43 +0800

    note

--- note.py
+++ note.py
@@ -2,7 +2,18 @@
 # False Value in Python: False, 0, '', None (More to come)
 # True Value in Python: Anything else
 
-#三目运算符：>>> x = 0
+# 三目运算符：>>> x = 0
 #                     >>> abs(1/x if x != 0 else 0)
 
-
+# Naming Tips
+# Names typically don’t matter for correctness but they matter a lot for composition
+# Names should convey the meaning or purpose of the values to which they are bound.
+# The type of value bound to the name is best documented in a function's docstring.
+# Function names typically convey their effect (print), their behavior (triple), or the value returned (abs).
+# Repeated compound expressions
+# Meaningful parts of complex expressions
+# Names can be long if they help document your code
+# Names can be short if they represent generic quantities: counts, arbitrary functions, arguments to mathematical operations, etc.
+# n, k, i - Usually integers
+# x, y, z - Usually real numbers
+# f, g, h - Usually functions
